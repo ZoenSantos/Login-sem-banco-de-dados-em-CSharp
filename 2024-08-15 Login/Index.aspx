8a681ea5664eaa03e0b64f52ff33a9162dd2d330
ZoenSantos <125597511+ZoenSantos@users.noreply.github.com>
2024-09-03 08:23:19 -0300

    Add files via upload

---
+++ 2024-08-15 Login/Index.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="_2024_08_15_Login.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <link rel="stylesheet" href="cor.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <asp:Label ID="lblInicio" runat="server" Text="Bem-Vindo a página inicial"></asp:Label>
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ 2024-08-15 Login/Login.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2024_08_15_Login
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            if(Login1.UserName == "legal" && Login1.Password == "senha boa")
+            {
+                e.Authenticated = true;
+                System.Web.Security.FormsAuthentication.RedirectFromLoginPage(Login1.UserName, false);
+                Response.Redirect("~/Index.aspx");
+            }
+            else
+            {
+                e.Authenticated = false;
+            }
+        }
+    }
+}
